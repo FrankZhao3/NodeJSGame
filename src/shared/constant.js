a940bf839e0eef78831bcc3ae6f1a600d28cb484
FrankZhao3 <hpzhao3@berkeley.edu>
2020-06-03 21:23:17 -0700

    changed player number

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -4,7 +4,7 @@ module.exports = {
     INIT_BLOCK_NUM: 10,
     INIT_CHAIR_NUM: 10,
     REWARD_POINT: 10,
-    MAX_PLAYER_NUM: 2,
+    MAX_PLAYER_NUM: 4,
     PENALTY_POINT: -5,
     TOTAL_TIME: 120,
     FIRST_POSITION: {X: 50, Y:50},
